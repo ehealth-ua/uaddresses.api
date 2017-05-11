fc8b12f949dbb4b02ae00261ef65254c8d7726aa
Andrew Dryga <andrew@dryga.com>
2017-05-11 13:18:11 +0300

    Fixed credo config

--- config/.credo.exs
+++ config/.credo.exs
@@ -4,7 +4,7 @@
       name: "default",
       files: %{
         included: ["lib/"],
-        excluded: ["lib/uaddresses/tasks.ex"]
+        excluded: ["lib/uaddresses_api/tasks.ex"]
       },
       checks: [
         {Credo.Check.Design.TagTODO, exit_status: 0},
