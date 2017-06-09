30d7e47b2f77f4c0d90fc4b6598f549c7ed5266d
MBatrak <maxb.edenlab@outlook.com>
2017-06-09 16:17:42 +0300

    Fixed settlements migration (#6)

--- priv/repo/migrations/20170609110044_change_settlements_table.exs
+++ priv/repo/migrations/20170609110044_change_settlements_table.exs
@@ -4,10 +4,8 @@ defmodule Uaddresses.Repo.Migrations.ChangeSettlementsTable do
   def change do
     alter table(:settlements) do
       add :type, :string, size: 1
-      remove :name
-      add :name, :string, null: false
-      remove :district_id
-      add :district_id, :uuid
+      modify :name, :string, null: false
+      modify :district_id, :uuid
       add :parent_settlement, :uuid
     end
   end
