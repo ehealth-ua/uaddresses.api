c753c3bacb40b93917d1da1b605524457415ac91
Marina Krasnova <marinakrasnovatr81@gmail.com>
2019-01-28 16:56:07 +0200

    Git ops (#54)

--- config/config.exs
+++ config/config.exs
@@ -2,24 +2,33 @@
 # and its dependencies with the aid of the Mix.Config module.
 use Mix.Config
 
-config :uaddresses_api,
-  namespace: Uaddresses,
-  ecto_repos: [Uaddresses.Repo]
+# By default, the umbrella project as well as each child
+# application will require this configuration file, ensuring
+# they all use the same configuration. While one could
+# configure all applications here, we prefer to delegate
+# back to each application for organization purposes.
 
-# This configuration file is loaded before any dependency and
-# is restricted to this project.
+config :git_ops,
+  mix_project: Uaddresses.MixProject,
+  changelog_file: "CHANGELOG.md",
+  repository_url: "https://github.com/edenlabllc/uaddresses.api/",
+  types: [
+    # Makes an allowed commit type called `tidbit` that is not
+    # shown in the changelog
+    tidbit: [
+      hidden?: true
+    ],
+    # Makes an allowed commit type called `important` that gets
+    # a section in the changelog with the header "Important Changes"
+    important: [
+      header: "Important Changes"
+    ]
+  ],
+  # Instructs the tool to manage your mix version in your `mix.exs` file
+  # See below for more information
+  manage_mix_version?: true,
+  # Instructs the tool to manage the version in your README.md
+  # Pass in `true` to use `"README.md"` or a string to customize
+  manage_readme_version: "README.md"
 
-# General application configuration
-# Configures the endpoint
-config :uaddresses_api, Uaddresses.Web.Endpoint,
-  url: [host: "localhost"],
-  secret_key_base: "kCj2tpqzsnxcsciNm5JLDnUgJChdtFCO5RMyIlnUfSw1bhxxgYGvM/OX3v2mosnU",
-  render_errors: [view: EView.Views.PhoenixError, accepts: ~w(json)]
-
-# Configures Elixir's Logger
-config :logger, :console,
-  format: "$message\n",
-  handle_otp_reports: true,
-  level: :info
-
-import_config "#{Mix.env()}.exs"
+import_config "../apps/*/config/config.exs"
