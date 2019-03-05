8f2c8fe643bda76e44d0d222bbfd713ff4caa18d
Denis Klimenko <denis.klimenko.dx@gmail.com>
2019-03-05 12:19:56 +0200

    fix entities render timestamps on rpc (#78)

--- apps/uaddresses_api/lib/uaddresses_api/web/views/region_view.ex
+++ apps/uaddresses_api/lib/uaddresses_api/web/views/region_view.ex
@@ -1,25 +1,23 @@
 defmodule Uaddresses.Web.RegionView do
   use Uaddresses.Web, :view
 
+  @fields ~w(id name koatuu)a
+
   def render("index.json", %{regions: regions}) do
     render_many(regions, __MODULE__, "region.json")
   end
 
   def render("index.rpc.json", %{regions: regions}) do
-    render_many(regions, __MODULE__, "region.json")
+    render_many(regions, __MODULE__, "region.rpc.json")
   end
 
   def render("show.json", %{region: region}) do
     render_one(region, __MODULE__, "region.json")
   end
 
-  def render("region.json", %{region: region}) do
-    %{
-      id: region.id,
-      name: region.name,
-      koatuu: region.koatuu
-    }
-  end
+  def render("region.json", %{region: region}), do: Map.take(region, @fields)
+
+  def render("region.rpc.json", %{region: region}), do: Map.take(region, @fields ++ [:inserted_at, :updated_at])
 
   def render("list_districts.json", %{districts: districts}) do
     render_many(districts, Uaddresses.Web.DistrictView, "district_by_region.json")
